35249d8e52937be0146522ca838e3bb2fc13df9e
Damian Wilkołek <dwilkolek@protonmail.com>
2019-11-13 02:33:30 +0100

    temporary menu fix.

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuOption } from 'src/app/model/menu-option.model';
 
 @Component({
   selector: 'app-main',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.less']
 })
 export class MainComponent implements OnInit {
-
+  menu: MenuOption[] = [
+    {
+      name: 'Constructions',
+      path: 'constructions',
+      suboptions: [
+        {
+          path: 'rod-finder',
+          name: 'Rod Finder'
+        },
+        {
+          path: 'roof-pressure-coefficients',
+          name: 'Roof Pressure Coefficients'
+        }
+      ]
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
